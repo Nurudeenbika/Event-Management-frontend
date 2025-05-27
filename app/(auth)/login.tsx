e9465ce288dec8a2c5a9703d48df9a229adaec68
Nurudeenbika <nurudeenbika@gmail.com>
2025-05-27 12:46:53 +0100

    fixes in register and login pages

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  Appearance,
   View,
   Text,
   TextInput,
@@ -8,11 +9,21 @@ import {
 } from "react-native";
 import { router } from "expo-router";
 import { useAuth } from "@/src/contexts/AuthContext";
+import { Colors } from "../../constants/Colors";
+import Animated, { FadeInUp } from "react-native-reanimated";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const colorScheme = Appearance.getColorScheme();
+
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
+
+  const styles = createStyles(theme, colorScheme);
 
   const handleLogin = async () => {
     try {
@@ -25,24 +36,48 @@ export default function LoginScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Animated.View
+        entering={FadeInUp.duration(800)}
+        style={styles.logoContainer}
+      >
+        <MaterialIcons name="event" size={28} color={theme.text} />
+        <Text style={styles.brand}>Vasrefil Event Management</Text>
+      </Animated.View>
+      <Animated.Text
+        entering={FadeInUp.delay(300).duration(800)}
+        style={styles.title}
+      >
+        Login
+      </Animated.Text>
+      {/* <Text style={styles.title}>Login</Text> */}
 
       <TextInput
         style={styles.input}
         placeholder="Email"
+        placeholderTextColor={theme.placeholder}
         value={email}
         onChangeText={setEmail}
         keyboardType="email-address"
         autoCapitalize="none"
       />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Password"
+          placeholderTextColor={theme.placeholder}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Feather
+            name={showPassword ? "eye" : "eye-off"}
+            size={20}
+            color={theme.text}
+          />
+        </TouchableOpacity>
+      </View>
 
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
@@ -55,40 +90,83 @@ export default function LoginScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 20,
-    backgroundColor: "#fff",
+function createStyles(
+  theme: {
+    text?: string;
+    background: any;
+    tint?: string;
+    icon?: string;
+    tabIconDefault?: string;
+    tabIconSelected?: string;
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 30,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ddd",
-    padding: 10,
-    marginBottom: 15,
-    borderRadius: 5,
-  },
-  button: {
-    backgroundColor: "#007AFF",
-    padding: 15,
-    borderRadius: 5,
-    alignItems: "center",
-    marginBottom: 15,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  linkText: {
-    textAlign: "center",
-    color: "#007AFF",
-  },
-});
+  colorScheme: string | null | undefined
+) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      padding: 20,
+      backgroundColor: theme.background,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: theme.text,
+      textAlign: "center",
+      marginBottom: 30,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: colorScheme === "dark" ? "papayawhip" : "#000",
+      padding: 10,
+      color: theme.text,
+      marginBottom: 15,
+      borderRadius: 5,
+    },
+    passwordContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      borderWidth: 1,
+      borderColor: colorScheme === "dark" ? "papayawhip" : "#000",
+      paddingHorizontal: 10,
+      borderRadius: 5,
+      marginBottom: 15,
+    },
+    passwordInput: {
+      flex: 1,
+      paddingVertical: 10,
+      color: theme.text,
+    },
+    button: {
+      backgroundColor: "#007AFF",
+      padding: 15,
+      borderRadius: 5,
+      alignItems: "center",
+      marginBottom: 15,
+    },
+    logoContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      marginBottom: 10,
+      gap: 8,
+    },
+    brand: {
+      fontSize: 21,
+      fontWeight: "800",
+      textTransform: "uppercase",
+      textAlign: "center",
+      marginBottom: 8,
+      color: theme.text,
+    },
+    buttonText: {
+      color: theme.text,
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+    linkText: {
+      textAlign: "center",
+      color: "#007AFF",
+    },
+  });
+}
